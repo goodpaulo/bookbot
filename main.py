11b720c0d481c0b92648780784bf65b11dde7576
goodpaulo <plastra2@gmail.com>
2025-09-09 19:19:43 -0400

    added more books

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from stats import get_num_words
-from stats import get_num_characters
+import sys
+from stats import get_num_words, get_num_characters, sort_dict
 
 def get_book_text(filepath):
-    with open(filepath) as f:
+    with open(filepath, "r", encoding="utf-8") as f:
         return f.read()
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+
+    print("----------- Word Count ----------")
+    get_num_words(text)
+
+    print("--------- Character Count -------")
+    character_count = get_num_characters(text)
+    sorted_characters = sort_dict(character_count)
+
+    for item in sorted_characters:
+        char = item["char"]
+        num = item["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
+
+    print("============= END ===============")
+
 
 main()
-
-get_num_words(get_book_text("books/frankenstein.txt"))
-
-character_count = get_num_characters(get_book_text("books/frankenstein.txt"))
-
-print(character_count)
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_num_words(book_text):
     number_of_words = len(book_text.split())
-    print(f"{number_of_words} words found in the document")
+    print(f"Found {number_of_words} total words")
 
 def get_num_characters(book_text):
     book_dict = {}
@@ -11,3 +11,13 @@ def get_num_characters(book_text):
         else:
             book_dict[character] = 1
     return book_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dictionary):
+    dict_list = []
+    for key, value in dictionary.items():
+        dict_list.append({"char": key, "num": value})
+    dict_list.sort(reverse=True, key = sort_on)
+    return dict_list
